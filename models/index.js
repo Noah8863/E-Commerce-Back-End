7508da3d0aaa2e58c2c03e5c7533d9be460f4c86
Noah8863 <stacknoah99@gmail.com>
2022-06-20 19:39:20 -0600

    added the ids to all the model js files

--- models/index.js
+++ models/index.js
@@ -4,17 +4,35 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
+// Products belongs to Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id'
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id'
+});
 
-// Products belongToMany Tags (through ProductTag)
+// Product belongs to many Tags through ProductTag
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+    foreignKey: 'product_id'
+  },
+});
 
-// Tags belongToMany Products (through ProductTag)
+// Tag belongs to many Products through ProductTag
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+    foreignKey: 'tag_id'
+  },
+});
 
 module.exports = {
   Product,
   Category,
   Tag,
-  ProductTag,
+  ProductTag
 };
